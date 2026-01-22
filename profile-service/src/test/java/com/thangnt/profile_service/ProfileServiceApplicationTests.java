81c6234f9fb4652b9d85aa2468bd40e14cee0bef
thangnt <nguyenthienthang2002@gmail.com>
2026-01-22 17:13:53 +0700

    init complete identity service + init profile service

---
+++ profile-service/src/test/java/com/thangnt/profile_service/ProfileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thangnt.profile_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
